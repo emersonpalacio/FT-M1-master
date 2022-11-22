507349505962dc8fa1c2427e564d682ac5d007f3
junior mojica <juniormojica26@gmail.com>
2022-11-21 22:55:18 -0500

    1/2 homework done

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,6 +1,24 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num){ /*
+1. convertir el str que nos pasan en un numero para poder hacer operaciones
+2.posicionar los numeros en un array para iterar sobre cada uno y realizar la transformacion
+3. sumar el resultado para devolverlo como solucion
+*/
+let array= num.split("")
+
+let j=0;
+let suma = 0;
+for (let i = array.length-1; i >=0 ;i--) { 
+
+    const element = array[i]
+    console.log(element);
+    suma+= element*Math.pow(2,j)
+     j++
+}
+console.log(suma);
+return suma;
+}
 
 function DecimalABinario(num) {}
 
